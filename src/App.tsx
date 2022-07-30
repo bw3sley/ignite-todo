3df858895f8e7abd5458bc2bf280b93f2a3d8b2a
Wesley Bernardes <bw3sley@gmail.com>
2022-07-29 21:20:00 -0300

    :sparkles: feat: create Task component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { Header } from "./components/Header";
 import { Form } from "./components/Form";
 import { Info } from "./components/Info";
+import { EmptyContent } from "./components/EmptyContent";
+import { Task } from "./components/Task";
 
 import "./global.css";
 
 import styles from './App.module.css';
+
+const tasks = [
+  {
+    id: "1",
+    content: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer."
+  }
+]
 
 export function App() {
   return (
@@ -15,6 +24,8 @@ export function App() {
         <Form />
 
         <Info />
+
+        { tasks.length == 0 ? <EmptyContent /> : <Task /> }
       </main>
     </>
   )
